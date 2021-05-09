685f061a204ae255e776d5cde84e724bd22a3cc1
Suto, Evelyne <evelyne.suto@garmin.com>
2021-05-09 15:32:34 +0300

    Initial Commit

---
+++ FileImportApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FileImportApp
+//
+//  Created by Suto, Evelyne on 09/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FileImportApp/FileImportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileImportAppApp.swift
+//  FileImportApp
+//
+//  Created by Suto, Evelyne on 09/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FileImportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
