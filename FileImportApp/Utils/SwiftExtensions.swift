75721867b87a1dec306894220f926b516edaec2d
evelyne.suto <evelyne.suto@softvision.com>
2023-02-01 09:58:31 +0200

    update color

--- FileImportApp/Utils/SwiftExtensions.swift
+++ FileImportApp/Utils/SwiftExtensions.swift
@@ -16,18 +16,15 @@ extension UTType {
 //https://stackoverflow.com/questions/29779128/how-to-make-a-random-color-with-swift
 extension UIColor {
     class func random(with seed: String) -> UIColor {
-        var total: Int = 0
-        for u in seed.unicodeScalars {
-            total += Int(UInt32(u))
-        }
+        let total: Int = seed.unicodeScalars.map { Int(UInt32($0)) }.reduce(0, +)
         
-        srand48(total * 200)
+        srand48(total * 255)
         let r = CGFloat(drand48())
         
         srand48(total)
         let g = CGFloat(drand48())
         
-        srand48(total / 200)
+        srand48(total / 255)
         let b = CGFloat(drand48())
         
         return UIColor(red: r, green: g, blue: b, alpha: 1)
